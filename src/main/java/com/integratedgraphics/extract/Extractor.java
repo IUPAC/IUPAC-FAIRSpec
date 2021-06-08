e69214473b79f952eadfbe9942c3ae479c71e861
hansonr <hansonr@STO28628W.ad.stolaf.edu>
2021-06-08 05:48:41 -0500

    Extractor working to iteratively open and list zip file contents.

--- src/main/java/com/integratedgraphics/extract/Extractor.java
+++ src/main/java/com/integratedgraphics/extract/Extractor.java
@@ -1,24 +1,45 @@
 package com.integratedgraphics.extract;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.iupac.fairspec.api.IFSObjectAPI;
+import org.iupac.fairspec.api.IFSObjectAPI.ObjectType;
+import org.iupac.fairspec.common.IFSException;
+import org.iupac.fairspec.common.IFSFindingAid;
 
 import com.integratedgraphics.util.Util;
 
 import javajs.util.JSJSONParser;
 import javajs.util.Lst;
 import javajs.util.PT;
+import javajs.util.Rdr;
 
 public class Extractor {
 
 
+	public Extractor() {
+		clearZipCache();
+	}
+	
 	private String extractVersion;
 	private List<String> objects;
+	private IFSFindingAid findingAid;
+	private String sourceDir;
 
 	public List<String> getObjectsForFile(File ifsExtractScript) throws IOException {
 		System.out.println("Extracting " + ifsExtractScript.getAbsolutePath());
@@ -106,20 +127,299 @@ public class Extractor {
 		
 	}
 
-	public void extractObjects(File targetDir) {
-		
-		// [
-		// "{IFS.finding.aid.source.data.uri::https://pubs.acs.org/doi/suppl/10.1021/acs.orglett.0c00571/suppl_file/ol0c00571_si_002.zip}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/{IFS.structure.representation.mol.2d::{id}.mol}"
-		// "{IFS.finding.aid.source.data.uri::https://pubs.acs.org/doi/suppl/10.1021/acs.orglett.0c00571/suppl_file/ol0c00571_si_002.zip}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/{IFS.nmr.representation.vender.dataset::{IFS.nmr.param.expt::*}-NMR.zip}"
-		// "{IFS.finding.aid.source.data.uri::https://pubs.acs.org/doi/suppl/10.1021/acs.orglett.0c00571/suppl_file/ol0c00571_si_002.zip}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/HRMS.zip|{IFS.ms.representation.pdf::**/*.pdf}"
-		// ]
+	public Set<String> extractObjects(File targetDir) throws IFSException, IOException {
 
 		
+		// "{IFS.finding.aid.source.data.uri::https://pubs.acs.org/doi/suppl/10.1021/acs.orglett.0c00571/suppl_file/ol0c00571_si_002.zip}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/{IFS.structure.representation.mol.2d::{id}.mol}"
+		// "{IFS.finding.aid.source.data.uri::https://pubs.acs.org/doi/suppl/10.1021/acs.orglett.0c00571/suppl_file/ol0c00571_si_002.zip}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/{IFS.structure.representation.mol.2d::{id}.mol}"
+
+// [parse first node]
+		
+		// {IFS.finding.aid.source.data.uri::https://pubs.acs.org/doi/suppl/10.1021/acs.orglett.0c00571/suppl_file/ol0c00571_si_002.zip}
+
+// [start a new finding aid and download this resource]
+		
+		// |
+		
+// [get file list]
+				
+		
+		//(\Q^FID for Publication/\E)(\Q{id=IFS.structure.param.compound.id::*}\E)(\Q.zip\E)
+	
+// [pass to StructureIterator]		
+// [find matches and add structures to finding aid structure collection]
+		
+		// |
+
+// [get file list]
+		// [for each structure...]
+		
+		// \Q{id}/\E(\Q{IFS.structure.representation.mol.2d::{id}.mol}/E)"
+
+		// add this representation to this structure 
+
+		
+		
+		
+		// "{IFS.finding.aid.source.data.uri::https://pubs.acs.org/doi/suppl/10.1021/acs.orglett.0c00571/suppl_file/ol0c00571_si_002.zip}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/{IFS.nmr.representation.vender.dataset::{IFS.nmr.param.expt::*}-NMR.zip}"
+
+		// [parse first node]
+				
+		// {IFS.finding.aid.source.data.uri::https://pubs.acs.org/doi/suppl/10.1021/acs.orglett.0c00571/suppl_file/ol0c00571_si_002.zip}
+
+		// [start a new finding aid and download this resource]
+				
+				// |
+				
+		// [get file list]
+			
+				//FID for Publication/{id=IFS.structure.param.compound.id::*}.zip     
+		
+		// [pass to StructureIterator]
+		// [find matches and add structures to finding aid structure collection	if necessary	
+				
+				// |                 
+
+		// [get file list]
+				
+				// {id}/{IFS.nmr.representation.vender.dataset::{IFS.nmr.param.expt::*}-NMR.zip}
+
+		// [pass to SpecDataIterator]
+		// [find matches and add NMR spec data to finding aid spec data collection; also add struc+spec to finding aid StrucSpecCollection]	
+
+		// bruker files identified with / just before vendor dataset closing }
+		// standard ^....$
+		// * or / at end just removes $
+		// *. becomes [^.]
+		// .  becomes [.]
+		// /**/*.pdf  becomes /(?:[^/]+/)*).+\Q.pdf\E
+
+//		String s = "test/ok/here/1c.pdf";    // test/**/*.pdf
+//		Pattern p = Pattern.compile("^\\Qtest\\E/(?:[^/]+/)*(.+\\Q.pdf\\E)$");
+//		Matcher m = p.matcher(s);
+//		System.out.println(m.find() ? m.groupCount() + " " + m.group(0) + " -- " + m.group(1) : "");
+		
+		System.out.println("=====");
+		
+		if (sourceDir != null)
+			System.out.println("extractObjects from " + sourceDir);
 		System.out.println("extractObjects to " + targetDir.getAbsolutePath());
+		
+		int[] pt = new int[1];
+		
+		for (int i = 0; i < objects.size(); i++) {
+			String sObj = objects.get(i);
+			System.out.println("found object " + sObj);
+			pt[0]= 0;
+			String sAid = getValue(sObj, "IFS.finding.aid.object", pt);
+			if (sAid == null)
+				throw new IFSException("no IFS.finding.aid.object:" + sObj);
+			pt[0] = 0;
+			String sUrl = getValue(sAid, "IFS.finding.aid.source.data.uri", pt);
+			if (sUrl == null)
+				throw new IFSException("no IFS.finding.aid.source.data.uri:" + sAid);
+			String sData = sAid.substring(pt[0]);
+			sUrl = localizeURL(sUrl);	
+			if (findingAid == null)
+				findingAid = new IFSFindingAid(sObj);
+			System.out.println("opening " + sUrl);
+			Map<String, ZipEntry> map = getZipContents(sUrl);
+			if (findingAid.zipContents == null)
+				findingAid.zipContents = new LinkedHashMap<String, ZipEntry>();
+			findingAid.zipContents.putAll(map);
+			processObject(sData, map.keySet());
+		}
+		return findingAid.zipContents.keySet();
+	}
+
+	private void processObject(String sData, Set<String> set) {
+//		ParseObject po = new ParseObject(sData);
+//		for (String fname: set) {
+//			po.check(fname);
+//				
+//			}
+//		}
+	}
+
+	/**
+	 * For testing (or for whatever reason zip files are local or should not use the
+	 * given source paths), replace https://......./ with sourceDir/
+	 * 
+	 * @param sUrl
+	 * @return localized URL
+	 */
+	private String localizeURL(String sUrl) {
+		if (sourceDir != null) {
+		  int pt = sUrl.lastIndexOf("/");
+		  sUrl = sourceDir + sUrl.substring(pt);
+		}
+		return sUrl;
+	}
+
+	private String getValue(String sObj, String key, int[] pt) throws IFSException {
+		key = "{" + key + "::";
+		int p = sObj.indexOf(key, pt[0]);
+		if (p < 0)
+			return null;
+		int q = -1;
+		int nBrace = 1;
+		p += key.length();
+		int len = sObj.length();
+		for (int i = p; i < len && nBrace > 0; i++) {
+			switch (sObj.charAt(i)) {
+			case '{':
+				q = i;
+				nBrace++;
+				break;
+			case '}':
+				if (--nBrace < 0) {
+					throw new IFSException("unopened '}' in " + sObj + " at char " + i);
+				}
+				q = i;
+				break;
+			}
+		}
+		if (nBrace > 0) {
+			throw new IFSException("unclosed '{' in " + sObj + " at char " + q);
+		}
+		pt[0] = q;
+		return sObj.substring(p, pt[0]++);
+	}
+
+	/**
+	 * parse the object string for a known type
+	 * 
+	 * @param sObj
+	 * @param pt
+	 * @return ObjectType
+	 */
+	private ObjectType parseType(String sObj, int[] pt) {
+		int p = sObj.indexOf("{", pt[0]);
+		if (p != pt[0])
+			return null;
+		ObjectType type = null;
+		int p0 = p + 1;
+		for (int i = p0, len = sObj.length(); i < len; i++) {
+			switch (sObj.charAt(i)) {
+				case '{':
+				return null;
+				case '=':
+					p0 = i + 1;
+					break;
+				case ':':
+				    return IFSObjectAPI.getObjectTypeForName(sObj.substring(p0, i));
+			}
+		}
+		return null;
 	}
 	
 	
 	
+	private static Map<String, Map<String, ZipEntry>> htZipContents = new HashMap<>();
+	
+	static boolean debugging;
+
+	/**
+	 * zip contents caching can save time in complex loading.
+	 * 
+	 */
+	public static void clearZipCache() {
+		htZipContents.clear();
+	}
+
+//	/**
+//	 * Retrieve the ZipEntry for a file in a given zip file.
+//	 * 
+//	 * Note that in JavaScript only, the ZipEntry returned contains a reference to
+//	 * the underlying ByteArrayInputStream ultimately backing this zip file. This
+//	 * allows us to retrieve the file data directly from the ZipEntry using
+//	 * jsutil.getZipBytes(zipEntry).
+//	 * 
+//	 * Names starting with "." are taken as case-insensitive extensions
+//	 * 
+//	 * 
+//	 * @param zipFile
+//	 * @param fileName   zip entry name or lower-case extension starting with "."
+//	 * @param isContains fileName is a fragment of the entry name, not the other way
+//	 *                   around
+//	 * @return the ZipEntry for this file, possibly cached.
+//	 */
+//	private static ZipEntry findZipEntry(String zipFile, String fileName, boolean isContains) {
+//		Map<String, ZipEntry> contents = getZipContents(zipFile);
+//		if (contents == null)
+//			return null;
+//		if (!isContains) {
+//			return contents.get(fileName);
+//		}
+//		boolean isLCExt = fileName.startsWith(".");
+//		for (Entry<String, ZipEntry> entry : contents.entrySet()) {
+//			String key = entry.getKey();
+//			if ((isLCExt ? key.toLowerCase() : key).indexOf(fileName) >= 0)
+//				return entry.getValue();
+//		}
+//		return null;
+//	}
+
+	/**
+	 * Gets the contents of a zip file.
+	 * 
+	 * @param zipPath the path to the zip file
+	 * @return a set of file names in alphabetical order
+	 * @throws IOException 
+	 */
+	public static Map<String, ZipEntry> getZipContents(String zipPath) throws IOException {
+		URL url = new URL(zipPath);// getURLWithCachedBytes(zipPath); // BH carry over bytes if we have them
+									// already
+		Map<String, ZipEntry> fileNames = htZipContents.get(url.toString());
+		if (fileNames != null)
+			return fileNames;
+		// Scan URL zip stream for files.
+		fileNames = new LinkedHashMap<String, ZipEntry>();
+		htZipContents.put(url.toString(), fileNames);
+		return readZipContentsIteratively(url.openStream(), fileNames, "");
+	}
+
+	private static Map<String, ZipEntry> readZipContentsIteratively(InputStream is, Map<String, ZipEntry> fileNames, String baseName) throws IOException {
+		if (baseName.length() > 0)
+			System.out.println("opening " + baseName);
+		ZipInputStream zis = new ZipInputStream(is);
+		ZipEntry zipEntry = null;
+		int n = 0;
+		while ((zipEntry = zis.getNextEntry()) != null) {
+			if (!zipEntry.isDirectory() && zipEntry.getSize() == 0)
+				continue;
+			n++;
+			String fname = baseName + zipEntry.getName();
+			if (debugging)
+				System.out.println(fname);
+			fileNames.put(fname, zipEntry); // Java has no use for the ZipEntry, but JavaScript can read it.
+			if (fname.endsWith(".zip")) {
+				readZipContentsIteratively(zis, fileNames, fname + "|");
+			}
+		}
+		if (baseName.length() == 0)
+			zis.close();
+		return fileNames;
+	}
+
+	public void setSourceDir(String sourceDir) {
+		this.sourceDir = sourceDir;
+	}
+
+
+	
+	class ParseObject {
+
+		private String sData;
+		
+		private Pattern p;
+		private Matcher m;
+
+		public ParseObject(String sData) {
+			this.sData = sData;
+		}
+		
+	}
 	
 
 }
--- src/main/java/com/integratedgraphics/extract/ExtractorTest.java
+++ src/main/java/com/integratedgraphics/extract/ExtractorTest.java
@@ -2,6 +2,9 @@ package com.integratedgraphics.extract;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
+
+import org.iupac.fairspec.common.IFSException;
 
 /**
  * 
@@ -30,40 +33,166 @@ import java.io.IOException;
  */
 public class ExtractorTest extends Extractor {
 
-	private final static String[] testSet = {
-			"acs.joc.0c00770",
-			"acs.orglett.0c00571",
-			"acs.orglett.0c00624",
-			"acs.orglett.0c00755",
-			"acs.orglett.0c00788",
-			"acs.orglett.0c00874",
-			"acs.orglett.0c00967",
-			"acs.orglett.0c01022",
-			"acs.orglett.0c01043",
-			"acs.orglett.0c01153",
-			"acs.orglett.0c01197",
-			"acs.orglett.0c01277",
-			"acs.orglett.0c01297",
-	};
-	
-	public ExtractorTest(File ifsExtractScript, File targetDir) throws IOException {
+	public ExtractorTest(File ifsExtractScript, File targetDir, String sourceDir) throws IOException, IFSException {
 		// first create super.objects, a List<String>
 		getObjectsForFile(ifsExtractScript);
 		// now actually do the extraction.
-		extractObjects(targetDir);
+		if (sourceDir != null)
+			super.setSourceDir(sourceDir);
+		Set<String> set = extractObjects(targetDir);
+		System.out.println("found " + set.size() + " files");
+
 	}
 
 
+	/**
+	 * ACS/FigShare codes   /21947274  
+	 * 
+	 * for example: https://ndownloader.figshare.com/files/21947274
+	 */
+	private final static String[] testSet = {
+			"acs.joc.0c00770/22567817",  // 0 727 files; zips of bruker dirs + mnovas
+			"acs.orglett.0c00571/21975525", // 3212 files; zips of bruker zips and HRMS
+			"acs.orglett.0c00624/21947274", // 1143 files; 2 MANY bruker dirs
+			"acs.orglett.0c00755/22150197", // 3 MANY bruker dirs
+			"acs.orglett.0c00788/22125318", // 4 jdfs
+			"acs.orglett.0c00874/22233351", // 5 bruker dirs
+			"acs.orglett.0c00967/22111341", // 6 bruker dirs + jdfs
+			"acs.orglett.0c01022/22195341", // 7  many mnovas
+			"acs.orglett.0c01043/22232721", // 8  single mnova
+			"acs.orglett.0c01153/22284726,22284729", // 9 bruker dirs (1 MB)
+			"acs.orglett.0c01197/22491647", // 10 many mnovas
+			"acs.orglett.0c01277/22613762", // 11 bruker dirs
+			"acs.orglett.0c01297/22612484", // 12 bruker dirs
+	};
+	
 	public static void main(String[] args) {
-		// ./extract/ should be in the main Eclipse project directory.
-		String def = (args.length < 1 ? testSet[1] : null);
-		String script = (args.length < 1 ? "./extract/" + def + "/IFS-extract.json" : args[0]);
-		String targetDir = (args.length >= 2 ? args[1] : def == null ? new File(script).getParent() :  "c:/temp/ifs/" + def);
-		try {
-			new ExtractorTest(new File(script), new File(targetDir));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+		int i0 = 0;
+		int i1 = 0;
 		
+		debugging = true; // verbose listing of all files
+		
+		int failed = 0;
+		
+		for (int itest = (args.length == 0 ? i0 : 0); 
+				itest <= (args.length == 0 ? i1 : 0); 
+				itest++) {
+			
+//		String s = "test/ok/1c.nmr";
+//		Pattern p = Pattern.compile("^\\Qtest/ok/\\E(.+)\\Q.nmr\\E");
+//		Matcher m = p.matcher(s);
+//		System.out.println(m.find());
+//		String v = m.group(1);
+//		
+//		
+			String script, targetDir = null, sourceDir = null;
+			switch (args.length) {
+			default:
+			case 3:
+				sourceDir = args[2];
+			case 2:
+				targetDir = args[1];
+			case 1:
+				script = args[0];
+				break;
+			case 0:
+				String def = testSet[itest];
+				System.out.println("found Test " + itest + " " + def);
+				int pt = def.indexOf("/");
+				if (pt >= 0)
+					def = def.substring(0, pt);
+				script = "./extract/" + def + "/IFS-extract.json";
+				sourceDir = "file:///c:/temp/iupac/zip";
+				targetDir = "file:///c:/temp/iupac/ifs";
+			}
+
+			// ./extract/ should be in the main Eclipse project directory.
+
+			try {
+				new ExtractorTest(new File(script), new File(targetDir), sourceDir);
+			} catch (Exception e) {
+				failed++;
+				System.err.println("Exception " + e + " for test " + itest);
+				e.printStackTrace();
+			}
+
+		}
+		System.out.println ("DONE failed=" + failed);
 	}
+	
+//	found Test 0 acs.joc.0c00770/22567817
+//	2 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22567817}|{id=IFS.structure.param.compound.id::*}.zip|{IFS.nmr.representation.vender.dataset::{id}_{IFS.nmr.param.expt::*}/}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22567817}|{id=IFS.structure.param.compound.id::*}.zip|{IFS.nmr.representation.vender.dataset::{id}_mnova}}
+//	found 727 files
+//	found Test 1 acs.orglett.0c00571/21975525
+//	3 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/21975525}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/{IFS.structure.representation.mol.2d::{id}.mol}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/21975525}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/{IFS.nmr.representation.vender.dataset::{IFS.nmr.param.expt::*}-NMR.zip}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/21975525}|FID for Publication/{id=IFS.structure.param.compound.id::*}.zip|{id}/HRMS.zip|**/{IFS.ms.representation.pdf::*.pdf}}
+//	found 3212 files
+//	found Test 2 acs.orglett.0c00624/21947274
+//	1 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/21947274}|primary NMR data/{id=IFS.structure.param.compound.id::*}/{IFS.nmr.representation.vender.dataset::{id}-{IFS.nmr.param.expt::*}/}}
+//	found 1143 files
+//	found Test 3 acs.orglett.0c00755/22150197
+//	1 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22150197}|NMR/{id=IFS.structure.param.compound.id::*}/{IFS.nmr.representation.vender.dataset::{IFS.nmr.param.expt::*}/}}
+//	found 2150 files
+//	found Test 4 acs.orglett.0c00788/22125318
+//	1 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22125318}|primary NMR data/{IFS.nmr.representation.vender.dataset::{IFS.structure.param.compound.id::*}-{IFS.nmr.param.expt::*}.jdf}}
+//	found 82 files
+//	found Test 5 acs.orglett.0c00874/22233351
+//	1 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22233351}|NMR FID files/{id=IFS.structure.param.compound.id::*}/{IFS.nmr.representation.vender.dataset::{IFS.nmr.param.expt::*}/}*}
+//	found 1598 files
+//	found Test 6 acs.orglett.0c00967/22111341
+//	2 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22111341}|{id=IFS.structure.param.compound.id::*}//{IFS.nmr.representation.vender.dataset::{id}-{IFS.nmr.param.expt::*}/}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22111341}|{id=IFS.structure.param.compound.id::*}/{IFS.nmr.representation.vender.dataset::{id}-{IFS.nmr.param.expt::*}.jdf}}
+//	found 1354 files
+//	found Test 7 acs.orglett.0c01022/22195341
+//	2 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22195341}|NMR DATA/product/{IFS.nmr.representation.vender.dataset::{id=IFS.structure.param.compound.id::*}-{IFS.nmr.param.expt::*}.mnova}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22195341}|NMR DATA/starting material/{IFS.nmr.representation.vender.dataset::{id=IFS.structure.param.compound.id::*}-{IFS.nmr.param.expt::*}.mnova}}
+//	found 66 files
+//	found Test 8 acs.orglett.0c01043/22232721
+//	1 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22232721}|metadataNMR/{IFS.nmr.representation.vender.dataset::NMR spectra.mnova}}
+//	found 1 files
+//	found Test 9 acs.orglett.0c01153/22284726,22284729
+//	4 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22284726}|*.zip|{id=IFS.structure.param.compound.id::*}/|{IFS.nmr.representation.vender.dataset::{IFS.nmr.param.expt::*}/}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22284726}|*.zip|{id=IFS.structure.param.compound.id::*}/|{IFS.structure.representation.cdx::*.cdx}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22284729}|*.zip|{id=IFS.structure.param.compound.id::*}/|{IFS.nmr.representation.vender.dataset::{IFS.nmr.param.expt::*}/}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22284729}|*.zip|{id=IFS.structure.param.compound.id::*}/|{IFS.structure.representation.cdx::*.cdx}}
+//	found 3476 files
+//	found Test 10 acs.orglett.0c01197/22491647
+//	2 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22491647}|NMR DATA/products/{IFS.nmr.representation.vender.dataset::{id=IFS.structure.param.compound.id::*}.mnova}}
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22491647}|NMR DATA/Substrate/{IFS.nmr.representation.vender.dataset::{id=IFS.structure.param.compound.id::*}.mnova}}
+//	found 61 files
+//	found Test 11 acs.orglett.0c01277/22613762
+//	1 objects found
+//	found object {IFS.finding.aid.object::{IFS.nmr.representation.vender.dataset::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22613762}|alkylation original FID submitted/{id=IFS.structure.param.compound.id::*}-{IFS.nmr.param.expt::*}/**/*}}
+//	found 5372 files
+//	found Test 12 acs.orglett.0c01297/22612484
+//	1 objects found
+//	found object {IFS.finding.aid.object::{IFS.finding.aid.source.data.uri::https://ndownloader.figshare.com/files/22612484}|NMR fids/{IFS.nmr.representation.vender.dataset::{id=IFS.structure.param.compound.id::*}/{IFS.nmr.param.expt::*}/}}
+//	found 1495 files
+
+	
+//FigShare searching:
+//import requests as rq
+//from pprint import pprint as pp
+//
+//HEADERS = {'content-type': 'application/json'}
+//
+//r = rq.post('https://api.figshare.com/v2/articles/search', data='{"search_for": "university of sheffield", "page_size":20}', headers=HEADERS)
+//print(r.status_code)
+//results = r.json()
+//pp(results)
+	
 }
