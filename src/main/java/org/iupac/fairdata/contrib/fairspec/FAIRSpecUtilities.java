9dd6dc4a8759b4223cd46180b04fb2bf6036b391
hansonr <hansonr@stolaf.edu>
2022-12-26 08:46:20 -0600

    removes javajs dependency

--- src/main/java/org/iupac/fairdata/contrib/fairspec/FAIRSpecUtilities.java
+++ src/main/java/org/iupac/fairdata/contrib/fairspec/FAIRSpecUtilities.java
@@ -619,59 +619,55 @@ public class FAIRSpecUtilities {
 			setLogging(null, true);
 	}
 
-	  /**
-	   * 
-	   *  proper splitting, even for Java 1.3 -- if the text ends in the run,
-	   *  no new line is appended.
-	   * 
-	   * @param text
-	   * @param run
-	   * @return  String array
-	   */
-	  public static String[] split(String text, String run) {
-	    if (text.length() == 0)
-	      return new String[0];
-	    int n = 1;
-	    int i = text.indexOf(run);
-	    String[] lines;
-	    int runLen = run.length();
-	    if (i < 0 || runLen == 0) {
-	      lines = new String[1];
-	      lines[0] = text;
-	      return lines;
-	    }
-	    int len = text.length() - runLen;
-	    for (; i >= 0 && i < len; n++)
-	      i = text.indexOf(run, i + runLen);
-	    lines = new String[n];
-	    i = 0;
-	    int ipt = 0;
-	    int pt = 0;
-	    for (; (ipt = text.indexOf(run, i)) >= 0 && pt + 1 < n;) {
-	      lines[pt++] = text.substring(i, ipt);
-	      i = ipt + runLen;
-	    }
-	    if (text.indexOf(run, len) != len)
-	      len += runLen;
-	    lines[pt] = text.substring(i, len);
-	    return lines;
-	  }
+	/**
+	 * 
+	 * proper splitting, even for Java 1.3 -- if the text ends in the run, no new
+	 * line is appended.
+	 * 
+	 * @param text
+	 * @param run
+	 * @return String array
+	 */
+	public static String[] split(String text, String run) {
+		if (text.length() == 0)
+			return new String[0];
+		int n = 1;
+		int i = text.indexOf(run);
+		String[] lines;
+		int runLen = run.length();
+		if (i < 0 || runLen == 0) {
+			lines = new String[1];
+			lines[0] = text;
+			return lines;
+		}
+		int len = text.length() - runLen;
+		for (; i >= 0 && i < len; n++)
+			i = text.indexOf(run, i + runLen);
+		lines = new String[n];
+		i = 0;
+		int ipt = 0;
+		int pt = 0;
+		for (; (ipt = text.indexOf(run, i)) >= 0 && pt + 1 < n;) {
+			lines[pt++] = text.substring(i, ipt);
+			i = ipt + runLen;
+		}
+		if (text.indexOf(run, len) != len)
+			len += runLen;
+		lines[pt] = text.substring(i, len);
+		return lines;
+	}
 
-	  public static String getQuotedAttribute(String info, String name) {
-		    int i = info.indexOf(name + "=\"");
-		    if (i < 0)
-		    	return null;
-		    i += name.length() + 2;
-		    int pt = i;
-		    int len = info.length();
-		    while (++i < len && info.charAt(i) != '"')
-		      if (info.charAt(i) == '\\')
-		        i++;
-		    return info.substring(pt, i);
-		  }
+	public static String getQuotedAttribute(String info, String name) {
+		int i = info.indexOf(name + "=\"");
+		if (i < 0)
+			return null;
+		i += name.length() + 2;
+		int pt = i;
+		int len = info.length();
+		while (++i < len && info.charAt(i) != '"')
+			if (info.charAt(i) == '\\')
+				i++;
+		return info.substring(pt, i);
+	}
 
-	  static {
-	  System.out.println(getQuotedAttribute("<test aref=\"test\">", "aref"));
-	  }
-	  
 }
