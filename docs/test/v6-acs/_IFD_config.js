c5d4f42d2291c5d2e357e35ebddedc62b566cd34
bobhanson <hansonr@stolaf.edu>
2025-08-26 19:03:13 -0500

    removing test; adding docs/.nojekyyl

--- docs/test/v6-acs/_IFD_config.js
+++
@@ -1,8 +0,0 @@
-// IFD_config.js
-
-IFD.properties.baseDir =  "https://chemapps.stolaf.edu/iupac/examples2/v6-acs";
-IFD.properties.haveLocalFiles = true; 
-IFD.properties.imageDimensions = {width:40,height:40};
-IFD.properties.MAX_IMAGE_DIMENSIONS = {width:200, height:200};
-
-
