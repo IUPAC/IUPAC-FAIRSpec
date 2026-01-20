6ee43bcadc092545e23ae3a3f57ac2a745e15d3d
emmaclift <emmaclift@localhost>
2026-01-20 15:39:01 -0600

    Adding compound_name

--- stolaf/emma/compound_candidate_identifier.py
+++ stolaf/emma/compound_candidate_identifier.py
@@ -8,9 +8,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 
+
+
+import itertools
 # Read file paths
 
-with open("file_list_v6wwpzh7x.txt", "r") as f:
+with open("file_list.txt", "r") as f:
     paths = [line.strip() for line in f if line.strip()]
 
 path_parts = [list(Path(p).parts) for p in paths]
@@ -740,7 +743,7 @@ def list_useful_files(diff_df):
     # Dictionary to store files: {compound: {test_folder: [files]}}
     useful_file_map = {}
 
-    useful_extensions = ('.mol', '.jdf' ,'.mnova', 'fid', 'pdata')
+    useful_extensions = ('.mol', '.jdf' ,'.mnova', 'fid', 'pdata', '1r')
 
     for _, row in diff_df.iterrows():
         path_parts = row["compound_path"]
@@ -791,14 +794,147 @@ print(final_df[['compound', 'useful_files']])
 
 print(final_df)   
 
+final_df['compound'] = final_df['compound'].str.replace(' + ', '+', regex=False)         
 
 final_df.to_csv('output_v6wwpzh7x.csv', index=False)
     
 final_df.to_json('output_v6wwpzh7x.json', indent=4)
     
-    
+
 
 with open("extractor_v6wwpzh7x.txt", "w") as file:
   file.writelines(file_content)
   
+  
+  
+  
+  
+# trying to make it output the file paths
 
+unique_entries_set = set(itertools.chain.from_iterable(final_df['test_folders']))
+print(unique_entries_set)
+
+# take this and filter through this --> then convert the df output back to just file paths
+# will have to add more filepaths that don't just start at the compound name
+
+FILE_PATH_KEYWORDS = ['1r', 'fid', '1i', '.mol', 'jdf', '.mnova', 'acqus'] + list(unique_entries_set)
+
+
+
+#filtered_df_regex = diff_df[diff_df['compound_path'].str.contains(regex_pattern, na = False)]
+filtered_df_regex = diff_df[diff_df['compound_path'].apply(lambda x: any(k in x for k in FILE_PATH_KEYWORDS))]
+filtered_df_regex = diff_df[diff_df['compound_path'].apply(lambda x: x[-1] in FILE_PATH_KEYWORDS if x else False)]
+
+print(filtered_df_regex)
+
+
+filtered_df_regex['my_string'] = filtered_df_regex['compound_path'].str.join('/')
+
+print(filtered_df_regex['my_string'] )
+
+filtered_df_regex['my_string'].to_csv('output_string.csv', index=False)
+
+
+print(path_parts)
+
+
+# trying to do something that is wanted to do
+
+# tokens are / . ' ' 
+
+
+folders_to_remove = set()
+for path in diff_df['compound_path']:
+    if 'pdata' in path:
+        idx = path.index('pdata')
+        if idx > 0:
+            folders_to_remove.add(path[idx - 1])
+            
+def global_clean(path_list):
+    #if 'pdata' in path_list:
+    #    idx = path_list.index('pdata')
+    #    path_list = path_list[:max(0, idx - 1)]
+    
+    for folder in folders_to_remove:
+      if folder in path_list:
+        return path_list[:path_list.index(folder)]
+    return path_list
+    #return [folder for folder in path_list if folder not in folders_to_remove]
+
+diff_df['compound_path'] = diff_df['compound_path'].apply(global_clean)
+
+
+
+print(diff_df['compound_path'])
+long_string = "\n".join(diff_df['compound_path'].apply(lambda x: " ".join(x)))
+long_string = long_string.replace(" + ", "+")
+print(long_string)
+
+tokens = re.split(r'[ \n\.\/]+', long_string)
+tokens = [t for t in tokens if t]
+#print(tokens)
+counts_series = pd.Series(tokens).value_counts()
+
+
+#print(counts_series)
+
+with pd.option_context('display.max_rows', None, 
+                       'display.max_columns', None):
+                         print(counts_series)
+                         
+for val in final_df['compound']:
+  val = val.replace(" + ", "+")
+
+
+final_df['compound'] = final_df['compound'].str.replace(' + ', '+', regex=False)               
+
+# now to get the just compound names, compare the tokens with the identified folders
+# if a compound folder name contains multiple tokens, select the one where it is less frequent
+# OR filter out the most common token in the folder name 
+
+print(final_df)
+
+print(tokens)
+
+for value, count in counts_series.items():
+  print(f"value: {value}, count: {count}")
+  
+
+tokens_in_compound_folder_name = []
+for compound_folder in final_df['compound']:
+  print("compound_folder")
+  print(compound_folder)
+  max_count = 0
+  for value, count in counts_series.items():
+    if value in compound_folder:
+      if count > max_count:
+        max_token = value
+        max_count = count
+  tokens_in_compound_folder_name.append(max_token)
+  print(max_count)  
+  print(max_token)
+
+
+print(tokens_in_compound_folder_name)
+tokens_in_compound_folder_name_set = set(tokens_in_compound_folder_name)
+print(tokens_in_compound_folder_name_set)
+
+print(len(final_df['compound']))
+print(len(tokens_in_compound_folder_name))
+print(len(tokens_in_compound_folder_name_set))
+
+for token_found in tokens_in_compound_folder_name_set:
+  final_df['compound_name'] = final_df['compound']
+  # basically i want  a way to find if the token_found is occuring a lot, and if it is, then probably get rid of it
+  word_freq = tokens_in_compound_folder_name.count(token_found)
+  print(word_freq)
+  if word_freq > len(tokens_in_compound_folder_name)/2:
+    print("THIS IS THE BAD ONE:")
+    print(token_found)
+    final_df['compound_name'] = final_df['compound'].str.replace(token_found, "")
+
+
+
+print(final_df)
+
+final_df.to_csv('output_v6wwpzh7x.csv', index=False)
